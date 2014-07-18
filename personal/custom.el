702dd9ed0792ee2670a88d1c02efebf09f948bde
Thomas Latzko <thomas.latzko@kit.edu>
2014-07-18 19:18:19 +0200

    yasnippet rename

--- personal/custom.el
+++ personal/custom.el
@@ -4,9 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(coffee-tab-width 2)
  '(column-number-mode t)
  '(custom-safe-themes (quote ("e26780280b5248eb9b2d02a237d9941956fc94972443b0f7aeec12b5c15db9f3" default)))
- '(org-agenda-files (quote ("~/phd/memo/plan.org")))
+ '(org-agenda-files (quote ("~/work/experimental/sensitivity/sensitivity.org" "~/phd/memo/plan.org")))
  '(org-agenda-include-all-todo t)
  '(org-agenda-include-diary t)
  '(org-agenda-skip-deadline-if-done t)
@@ -34,4 +35,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "adobe" :slant normal :height 99 :width normal)))))
+ '(default ((t (:background nil)))))
